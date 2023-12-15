28e744af81f99da704cb74ec3910dadd2fae715c
iraklisg <iraklis.g@gmail.com>
2023-12-15 11:20:02 +0200

    Update hylo development helper script

--- .local/bin/hylo.sh
+++ .local/bin/hylo.sh
@@ -26,6 +26,63 @@ function display_help() {
     exit 1
 }
 
+# Parse server name and return project, datahub and environment
+# 
+# Servername format: hylo-<project>[-datahub]-<environment>
+# Example: hylo-tanzdigital-staging
+# Example: hylo-tanzdigital-datahub-staging
+#
+function set_project_details() {
+    local server_name="$1"
+    local - a parts
+    parts=("${(@s/-/)server_name#hylo-}")
+
+    if [[ ${#parts[@]} -eq 3 ]]; then
+        project="${parts[1]}"
+        datahub=true
+        environment="${parts[3]}"
+    elif [[ ${#parts[@]} -eq 2 ]]; then
+        project="${parts[1]}"
+        datahub=false
+        environment="${parts[2]}"
+    else
+        echo -e "${_color_red}Invalid server name format${_nc}"
+        exit 1
+    fi
+}
+
+# Set remote project remote_user and remote_project_dir
+#
+function set_remote_details() {
+    if [[ "$datahub" == false ]]; then
+        remote_user="$project"
+        remote_project_dir="$project"
+    else
+        remote_user="data_hub"
+        remote_project_dir="data_hub"
+    fi
+}
+
+function run_git_command_via_ssh() {
+    local server_name="$1"
+    local command="$2"
+
+    local project
+    local datahub
+    local environment
+
+    local remote_user
+    local remote_project_dir
+
+    set_project_details "$server_name"
+
+    set_remote_details
+
+    echo "${_color_green}Connect to ${_bold}$server_name as $remote_user${_nc}\n"
+
+    ssh "${server_name}" "su - ${remote_user} -c \"cd project/${remote_project_dir}/ && git ${command}\""
+}
+
 if [ "$#" -gt 0 ]; then
     if [ "$1" = "help" ]; then
         display_help
@@ -58,8 +115,18 @@ if [ "$#" -gt 0 ]; then
             -e PGPASSWORD=${DATABASE_PASSWORD} \
             postgis/postgis:16-3.4 psql -h ${DATABASE_HOST} -U ${DATABASE_USER} "$@"
 
+    elif [ "$1" = "checkcommit" ]; then
+        shift 1
+        local server_name="$@"
+        run_git_command_via_ssh "$server_name" "log -1"
+
+    elif [ "$1" = "checkbranch" ]; then
+        shift 1
+        local server_name="$@"
+        run_git_command_via_ssh "$server_name" "rev-parse --abbrev-ref HEAD"
+
     else
-        echo "Invalid argument: $1"
+        echo "Invalid argument: $@"
         display_help
     fi
 else
