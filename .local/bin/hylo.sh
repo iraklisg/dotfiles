24a29f2a567186d45374b1d2362b78ba0d25d5cd
Iraklis <iraklis.g@gmail.com>
2024-01-21 21:02:20 +0200

    Add new remote git commands hylo.sh development helper script

--- .local/bin/hylo.sh
+++ .local/bin/hylo.sh
@@ -28,6 +28,8 @@ function display_help() {
     echo "${_color_yellow}Server Commands:${_nc}"
     echo "  ${_color_green}hylo server:commit-check SERVER_NAME${_nc}   Check latest commit on remote server"
     echo "  ${_color_green}hylo server:branch-check SERVER_NAME${_nc}   Check current branch on remote server"
+    echo "  ${_color_green}hylo server:locale-check SERVER_NAME${_nc}   Check for untracked locale files on remote server"
+    echo "  ${_color_green}hylo server:locale-push SERVER_NAME${_nc}    Push locale files from remote server"
     echo
     echo "${_color_yellow}Browserstack Commands:${_nc}"
     echo "  ${_color_green}hylo bs:start SERVER_NAME${_nc}   Start broserstack local"
@@ -35,7 +37,7 @@ function display_help() {
     exit 1
 }
 
-# Parse server name and return project, datahub and environment
+# Parse server name and sets `project`, `datahub` and `environment` variables
 # 
 # Servername format: hylo-<project>[-datahub]-<environment>
 # Example: hylo-tanzdigital-staging
@@ -60,9 +62,12 @@ function set_project_details() {
     fi
 }
 
-# Set remote project remote_user and remote_project_dir
+# Sets `remote_user` and `remote_project_dir` variables
+# Accepts `datahub` boolean
 #
 function set_remote_details() {
+    local datahub="$1"
+
     if [[ "$datahub" == false ]]; then
         remote_user="$project"
         remote_project_dir="$project"
@@ -72,24 +77,25 @@ function set_remote_details() {
     fi
 }
 
-function run_git_command_via_ssh() {
+# Run command on remote server via ssh
+# Accepts server name and command
+#
+function run_command_via_ssh() {
     local server_name="$1"
     local command="$2"
 
     local project
     local datahub
     local environment
+    set_project_details "$server_name"
 
     local remote_user
     local remote_project_dir
+    set_remote_details "$datahub"
 
-    set_project_details "$server_name"
+    echo "${_color_blue}Connecting to ${_bold}$server_name as $remote_user${_nc}...\n"
 
-    set_remote_details
-
-    echo "${_color_green}Connect to ${_bold}$server_name as $remote_user${_nc}\n"
-
-    ssh "${server_name}" "su - ${remote_user} -c \"cd project/${remote_project_dir}/ && git ${command}\""
+    ssh "${server_name}" "su - ${remote_user} -c 'cd project/${remote_project_dir}/ && ${command}'"
 }
 
 if [ "$#" -gt 0 ]; then
@@ -127,12 +133,65 @@ if [ "$#" -gt 0 ]; then
     elif [ "$1" = "server:commit-check" ]; then
         shift 1
         local server_name="$@"
-        run_git_command_via_ssh "$server_name" "log -1"
+        local command="git log -1"
+
+        run_command_via_ssh "$server_name" "$command"
 
     elif [ "$1" = "server:branch-check" ]; then
         shift 1
         local server_name="$@"
-        run_git_command_via_ssh "$server_name" "rev-parse --abbrev-ref HEAD"
+        local command="git rev-parse --abbrev-ref HEAD"
+
+        run_command_via_ssh "$server_name" "$command"
+
+    elif [ "$1" = "server:locale-check" ]; then
+        shift 1
+        local server_name="$@"
+        local command="
+            if git status --porcelain | grep -q \"locale/.*LC_MESSAGES/django\\.\(po\\|mo\)\"; then
+                echo -e \"${_color_red}Untracked LC_MESSAGES files found.${_nc}\"
+            else
+                echo -e \"${_color_green}No untracked LC_MESSAGES files found.${_nc}\"
+            fi"
+
+        run_command_via_ssh "$server_name" "$command"
+    
+    elif [ "$1" = "server:locale-push" ]; then
+        shift 1
+        local server_name="$@"
+        local command="
+            if git status --porcelain | grep -q \"locale/.*LC_MESSAGES/django\\.\(po\\|mo\)\"; then
+                echo -e \"${_color_red}Untracked LC_MESSAGES files found.${_nc}\"
+
+                echo \"Attempt to merge remote changes.\"
+                # Fetch the latest changes without merging
+                git fetch origin
+
+                # Attempt to merge without committing
+                git merge --no-commit --no-ff origin/master
+
+                # Check if there are merge conflicts
+                if git ls-files -u | grep -q \"^\"; then
+                    echo \"Merge conflicts detected. Aborting merge.\"
+                    git merge --abort
+                    exit 1
+                else
+                    echo \"No conflicts. Proceeding.\"
+
+                    # Add the translation files
+                    git add locale/de/LC_MESSAGES/django.po locale/de/LC_MESSAGES/django.mo
+
+                    # Commit the changes
+                    git commit -m \"🌐 Translations\"
+
+                    # Push the changes
+                    git push
+                fi
+            else
+                echo \"No untracked LC_MESSAGES files found. No action taken.\"
+            fi"
+
+        run_command_via_ssh "$server_name" "$command"
 
     elif [ "$1" = "bs:start" ]; then
         if [[ -z "$BROWSERSTACK_ACCESS_KEY" ]]; then
@@ -142,7 +201,7 @@ if [ "$#" -gt 0 ]; then
         /opt/browserstack/BrowserStackLocal --key "$BROWSERSTACK_ACCESS_KEY"
 
     else
-        echo "Invalid argument: $@"
+        echo -e "${_color_red}Invalid argument: $@${_nc}"
         display_help
     fi
 else
